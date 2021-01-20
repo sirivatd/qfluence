6e1968c229271efc038cba2677b959553b46ac14
Don Sirivat <don@joinhandshake.com>
2021-01-19 23:16:15 -0500

    Update profile page

--- qfluence/LandingViewController.swift
+++ qfluence/LandingViewController.swift
@@ -55,7 +55,18 @@ class LandingViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         handle = Auth.auth().addStateDidChangeListener { (auth, user) in
             if user != nil {
-                self.performSegue(withIdentifier: "toMainApp", sender: self)
+                let ref = Database.database().reference(withPath: "users")
+                ref.child(user!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+                    let user = snapshot.value as? NSDictionary
+                    
+                    let firstName = user!["firstName"] as? String ?? ""
+                    let lastName = user!["lastName"] as? String ?? ""
+                    let emailAddress = user!["emailAddress"] as? String ?? ""
+                    let joinedAt = user!["timeCreated"] as? String ?? ""
+                    
+                    currentUser = UserObject(firstName: firstName, lastName: lastName, emailAddress: emailAddress, joinedAt: joinedAt)
+                    self.performSegue(withIdentifier: "toMainApp", sender: self)
+                })
             }
         }
     }
--- qfluence/ProfileViewController.swift
+++ qfluence/ProfileViewController.swift
@@ -21,26 +21,45 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var contactButton: UIButton!
     
     @IBAction func contactPressed(_ sender: Any) {
+        let email = "hello@qfluencer.com"
+        if let url = URL(string: "mailto:\(email)") {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
     }
     
     @IBAction func termsPressed(_ sender: Any) {
+        guard let url = URL(string: "https://qfluence.herokuapp.com") else { return }
+        UIApplication.shared.open(url)
     }
     @IBAction func darkModePressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
     }
-    @IBAction func influencerPressed(_ sender: Any) {
-    }
+
     @IBAction func logoutPressed(_ sender: Any) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-            self.dismiss(animated: true, completion: nil)
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
-        }
+        let alert = UIAlertController(title: "Signing Out", message: nil, preferredStyle: .alert)
+        alert.message = "Are you sure you want to sign out?"
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            let firebaseAuth = Auth.auth()
+            do {
+                try firebaseAuth.signOut()
+                self.dismiss(animated: true, completion: nil)
+            } catch let signOutError as NSError {
+                print("Error signing out: %@", signOutError)
+            }
+        }))
+
+        self.present(alert, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateGreeting()
         backdropView.layer.cornerRadius = 10.0
         backdropView.layer.borderWidth = 0.5
         backdropView.layer.borderColor = UIColor.clear.cgColor
@@ -54,32 +73,56 @@ class ProfileViewController: UIViewController {
         setupButtons()
     }
     
+    func updateGreeting() {
+        if currentUser != nil {
+            self.nameLabel.text = currentUser!.firstName
+            self.joinedAtLabel.text = "Member since " + currentUser!.joinedAt.prefix(10)
+        } else {
+            self.nameLabel.text = ""
+            self.joinedAtLabel.text = "Pick any of the following options"
+        }
+        
+        // update greeting header
+        var greeting: String = ""
+        let hour = Calendar.current.component(.hour, from: Date())
+
+        switch hour {
+        case 6..<12 : greeting = "Good morning,"
+        case 12 : greeting = "Good day,"
+        case 13..<17 : greeting = "Good afternoon,"
+        case 17..<22 : greeting = "Good evening,"
+        default: greeting = "Welcome back,"
+        }
+        
+        self.greetingText.text = greeting
+    }
+    
     func setupButtons() {
         self.tutorialButton.layer.shadowColor = UIColor.black.cgColor
         self.tutorialButton.layer.shadowOpacity = 1
         self.tutorialButton.layer.shadowOffset = .zero
-        self.tutorialButton.layer.shadowRadius = 10
+        self.tutorialButton.layer.shadowRadius = 5
         self.tutorialButton.layer.shadowPath = UIBezierPath(rect: self.tutorialButton.bounds).cgPath
         self.tutorialButton.layer.cornerRadius = 10.0
         
         self.contactButton.layer.shadowColor = UIColor.black.cgColor
         self.contactButton.layer.shadowOpacity = 1
         self.contactButton.layer.shadowOffset = .zero
-        self.contactButton.layer.shadowRadius = 10
+        self.contactButton.layer.shadowRadius = 5
         self.contactButton.layer.shadowPath = UIBezierPath(rect: self.contactButton.bounds).cgPath
         self.contactButton.layer.cornerRadius = 10.0
         
         self.darkModeButton.layer.shadowColor = UIColor.black.cgColor
         self.darkModeButton.layer.shadowOpacity = 1
         self.darkModeButton.layer.shadowOffset = .zero
-        self.darkModeButton.layer.shadowRadius = 10
+        self.darkModeButton.layer.shadowRadius = 5
         self.darkModeButton.layer.shadowPath = UIBezierPath(rect: self.darkModeButton.bounds).cgPath
         self.darkModeButton.layer.cornerRadius = 10.0
         
         self.termsButton.layer.shadowColor = UIColor.black.cgColor
         self.termsButton.layer.shadowOpacity = 1
         self.termsButton.layer.shadowOffset = .zero
-        self.termsButton.layer.shadowRadius = 10
+        self.termsButton.layer.shadowRadius = 5
         self.termsButton.layer.shadowPath = UIBezierPath(rect: self.termsButton.bounds).cgPath
         self.termsButton.layer.cornerRadius = 10.0
     }
