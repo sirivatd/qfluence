cd0df458eabf11e7305ed5051eed5a206d4f6745
Don Sirivat <don@joinhandshake.com>
2021-01-21 01:58:20 -0500

    Update explore page add lazy loading

--- qfluence/ExploreViewController.swift
+++ qfluence/ExploreViewController.swift
@@ -17,27 +17,35 @@ struct QuestionObject {
     let imageUrl: String
 }
 
+struct ExploreObject {
+    let imageUrl: String
+    let name: String
+    let videoUrl: String
+    let questionText: String
+    let videoKey: String
+}
+
 class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tutorialView: UIView!
     @IBOutlet weak var animationView: AnimationView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.videoURLs.count
+        return self.exploreObjects.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "exploreCell") as! ExploreTableViewCell
-//        cell.videoPlayerItem = AVPlayerItem.init(url: videoURLs[indexPath.row])
         cell.questionText.text = self.exploreObjects[indexPath.row].questionText
         cell.subtitleText.text = self.exploreObjects[indexPath.row].name
         cell.profilePicture.downloadImageFrom(link: self.exploreObjects[indexPath.row].imageUrl, contentMode: UIView.ContentMode.scaleAspectFill)
-//
+
         cell.profilePicture.layer.borderWidth = 1
         cell.profilePicture.layer.masksToBounds = false
         cell.profilePicture.layer.borderColor = UIColor.clear.cgColor
         cell.profilePicture.layer.cornerRadius = cell.profilePicture.frame.height/2
         cell.profilePicture.clipsToBounds = true
         
-        cell.configureCell(videoUrl: videoURLs[indexPath.row])
+        cell.configureCell(videoUrl: self.exploreObjects[indexPath.row].videoUrl)
         
         return cell
     }
@@ -50,73 +58,16 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
            return 0
     }
     
-    
-//    func playVideoOnTheCell(cell : ExploreTableViewCell, indexPath : IndexPath){
-//        cell.startPlayback()
-//    }
-//
-//    func stopPlayBack(cell : ExploreTableViewCell, indexPath : IndexPath){
-//        cell.stopPlayback()
-//    }
-
-//    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        print("end = \(indexPath)")
-//        if let videoCell = cell as? ExploreTableViewCell {
-//            videoCell.stopPlayback()
-//        }
-//    }
-//    
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//           let indexPaths = self.exploreTableView.indexPathsForVisibleRows
-//           var cells = [Any]()
-//           for ip in indexPaths!{
-//               if let videoCell = self.exploreTableView.cellForRow(at: ip) as? ExploreTableViewCell{
-//                   cells.append(videoCell)
-//               }
-//           }
-//           let cellCount = cells.count
-//           if cellCount == 0 {return}
-//           if cellCount == 1{
-//               if visibleIP != indexPaths?[0]{
-//                   visibleIP = indexPaths?[0]
-//               }
-//               if let videoCell = cells.last! as? ExploreTableViewCell{
-//                   self.playVideoOnTheCell(cell: videoCell, indexPath: (indexPaths?.last)!)
-//               }
-//           }
-//           if cellCount >= 2 {
-//               for i in 0..<cellCount{
-//                   let cellRect = self.exploreTableView.rectForRow(at: (indexPaths?[i])!)
-//                   let intersect = cellRect.intersection(self.exploreTableView.bounds)
-//   //                curerntHeight is the height of the cell that
-//   //                is visible
-//                   let currentHeight = intersect.height
-//                   print("\n \(currentHeight)")
-//                   let cellHeight = (cells[i] as AnyObject).frame.size.height
-//   //                0.95 here denotes how much you want the cell to display
-//   //                for it to mark itself as visible,
-//   //                .95 denotes 95 percent,
-//   //                you can change the values accordingly
-//                   if currentHeight > (cellHeight * 0.95){
-//                       if visibleIP != indexPaths?[i]{
-//                           visibleIP = indexPaths?[i]
-//                           if let videoCell = cells[i] as? ExploreTableViewCell{
-//                               self.playVideoOnTheCell(cell: videoCell, indexPath: (indexPaths?[i])!)
-//                           }
-//                       }
-//                   }
-//                   else{
-//                       if aboutToBecomeInvisibleCell != indexPaths?[i].row{
-//                           aboutToBecomeInvisibleCell = (indexPaths?[i].row)!
-//                           if let videoCell = cells[i] as? ExploreTableViewCell{
-//                               self.stopPlayBack(cell: videoCell, indexPath: (indexPaths?[i])!)
-//                           }
-//
-//                       }
-//                   }
-//               }
-//           }
-//       }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if self.exploreObjects.count - indexPath.row < 3 {
+            // fetch more
+            let currentCount = self.exploreObjects.count
+            var newObjects: [ExploreObject]?
+            
+            self.exploreObjects = self.exploreObjects + self.allExploreObjects.dropFirst(10)
+            self.exploreTableView.reloadData()
+        }
+    }
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         if self.tutorialView.isHidden {
@@ -130,41 +81,60 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
     var aboutToBecomeInvisibleCell = -1
     var avPlayerLayer: AVPlayerLayer!
     var videoURLs = Array<String>()
-    var exploreObjects = Array<SearchVideoResult>()
+    var exploreObjects = Array<ExploreObject>()
+    var allExploreObjects = Array<ExploreObject>()
     
     @IBOutlet weak var exploreTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchVideos()
-//        visibleIP = IndexPath.init(row: 0, section: 0)
         self.exploreTableView.isPagingEnabled = true
         animationView.contentMode = .scaleAspectFill
         animationView.loopMode = .loop
         animationView.play()
         
-//        NotificationCenter.default.addObserver(self,
-//                                               selector: #selector(self.appEnteredFromBackground),
-//                                               name: UIApplication.willEnterForegroundNotification, object: nil)
+        var frame = CGRect.zero
+        frame.size.height = .leastNormalMagnitude
+        self.exploreTableView.tableHeaderView = UIView(frame: frame)
     }
     
     func fetchVideos() {
-        let videoRef = Database.database().reference(withPath: "videos").queryLimited(toLast: 10)
+        let videoRef = Database.database().reference(withPath: "videos").queryLimited(toLast: 200)
         videoRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            let firebaseDispatch = DispatchGroup()
             for video in snapshot.children {
+                firebaseDispatch.enter()
                 if let snapshot = video as? DataSnapshot {
+                    let videoKey = snapshot.key
                     let dict = snapshot.value as? NSDictionary
                     let questionText = dict!["questionText"] as! String
                     let videoUrlString = dict!["videoUrl"] as! String
                     let influencerId = dict!["influencerId"] as! Int
                         
-                    self.findInfluencer(influencerId: influencerId, questionText: questionText, videoUrl: videoUrlString)
+                    self.findInfluencer(influencerId: influencerId, questionText: questionText, videoUrl: videoUrlString, videoKey: videoKey, dispatch: firebaseDispatch)
                 }
+            }
+            firebaseDispatch.notify(queue: .main) {
+                print("All done!")
+                print(self.allExploreObjects.count)
+                self.allExploreObjects = self.allExploreObjects.shuffled()
+                self.exploreObjects = Array(self.allExploreObjects.prefix(10))
+                print(self.exploreObjects)
+                self.exploreTableView.reloadData()
             }
         })
     }
     
-    func findInfluencer(influencerId: Int, questionText: String, videoUrl: String) {
+    func updateLocalData() {
+        self.allExploreObjects = self.allExploreObjects.shuffled()
+        self.exploreObjects = Array(self.allExploreObjects.dropFirst(20))
+        print(self.exploreObjects)
+        print(self.allExploreObjects)
+        self.exploreTableView.reloadData()
+    }
+    
+    func findInfluencer(influencerId: Int, questionText: String, videoUrl: String, videoKey: String, dispatch: DispatchGroup) {
         let ref = Database.database().reference(withPath: "influencers").child("\(influencerId)")
         ref.observeSingleEvent(of: .value, with: {(snapshot) in
             let influencer = snapshot.value as? NSDictionary
@@ -181,11 +151,11 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
                 name = firstName! + " " + lastName!
             }
             
-            let exploreObject = SearchVideoResult(imageUrl: imageUrl!, name: name!, videoUrl: videoUrl, questionText: questionText)
-            
-            self.exploreObjects.append(exploreObject)
-            self.videoURLs.append(videoUrl)
-            self.exploreTableView.reloadData()
+            let exploreObject = ExploreObject(imageUrl: imageUrl!, name: name!, videoUrl: videoUrl, questionText: questionText, videoKey: videoKey)
+            self.allExploreObjects.append(exploreObject)
+            dispatch.leave()
+//            self.videoURLs.append(videoUrl)
+//            self.exploreTableView.reloadData()
         })
     }
     
