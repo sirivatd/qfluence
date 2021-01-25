59ebe667d5b51f048e35d6d74ecc6742529ce848
Don Sirivat <don@joinhandshake.com>
2021-01-25 05:23:17 -0500

    Update layout for ipad

--- qfluence/ProfileViewController.swift
+++ qfluence/ProfileViewController.swift
@@ -15,11 +15,6 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var joinedAtLabel: UILabel!
     @IBOutlet weak var backdropView: UIView!
-    @IBOutlet weak var logoutButton: UIButton!
-    @IBOutlet weak var tutorialButton: UIButton!
-    @IBOutlet weak var darkModeButton: UIButton!
-    @IBOutlet weak var termsButton: UIButton!
-    @IBOutlet weak var contactButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +23,6 @@ class ProfileViewController: UIViewController {
         backdropView.layer.borderWidth = 0.5
         backdropView.layer.borderColor = UIColor.clear.cgColor
         backdropView.layer.masksToBounds = true
-        
-        logoutButton.layer.cornerRadius = 5.0
-        logoutButton.layer.borderWidth = 0.5
-        logoutButton.layer.borderColor = UIColor.clear.cgColor
-        logoutButton.layer.masksToBounds = true
-        
-        setupButtons()
     }
     
     func updateGreeting() {
@@ -59,36 +47,6 @@ class ProfileViewController: UIViewController {
         }
         
         self.greetingText.text = greeting
-    }
-    
-    func setupButtons() {
-        self.tutorialButton.layer.shadowColor = UIColor.black.cgColor
-        self.tutorialButton.layer.shadowOpacity = 1
-        self.tutorialButton.layer.shadowOffset = .zero
-        self.tutorialButton.layer.shadowRadius = 5
-        self.tutorialButton.layer.shadowPath = UIBezierPath(rect: self.tutorialButton.bounds).cgPath
-        self.tutorialButton.layer.cornerRadius = 10.0
-        
-        self.contactButton.layer.shadowColor = UIColor.black.cgColor
-        self.contactButton.layer.shadowOpacity = 1
-        self.contactButton.layer.shadowOffset = .zero
-        self.contactButton.layer.shadowRadius = 5
-        self.contactButton.layer.shadowPath = UIBezierPath(rect: self.contactButton.bounds).cgPath
-        self.contactButton.layer.cornerRadius = 10.0
-        
-        self.darkModeButton.layer.shadowColor = UIColor.black.cgColor
-        self.darkModeButton.layer.shadowOpacity = 1
-        self.darkModeButton.layer.shadowOffset = .zero
-        self.darkModeButton.layer.shadowRadius = 5
-        self.darkModeButton.layer.shadowPath = UIBezierPath(rect: self.darkModeButton.bounds).cgPath
-        self.darkModeButton.layer.cornerRadius = 10.0
-        
-        self.termsButton.layer.shadowColor = UIColor.black.cgColor
-        self.termsButton.layer.shadowOpacity = 1
-        self.termsButton.layer.shadowOffset = .zero
-        self.termsButton.layer.shadowRadius = 5
-        self.termsButton.layer.shadowPath = UIBezierPath(rect: self.termsButton.bounds).cgPath
-        self.termsButton.layer.cornerRadius = 10.0
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
